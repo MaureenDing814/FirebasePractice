f1df96fce27de454098ea0bf0306ec5e9b715c29
Maureen_Ding <md3474@tc.columbia.edu>
2017-03-08 20:24:51 -0500

    Added save to database

--- app/src/main/java/com/example/android/secret/MainActivity.java
+++ app/src/main/java/com/example/android/secret/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.android.secret;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void save(View view) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("secret");
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String userSecret = editText.getText().toString();
+        myRef.setValue(userSecret);
+
+    }
+
+    public void reveal(View view) {
+
+    }
+
 }
